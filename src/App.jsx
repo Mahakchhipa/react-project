551cb22e36cf5ae4ea2a0e657c7cb70797ac02fa
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-04-06 18:38:56 +0530

    Home page start

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-center text-5xl text-red-500 font-bold "> My React Project </h1>
+      <h1 className="text-center text-5xl text-green-500 font-bold ">
+        {" "}
+        My React Project{" "}
+      </h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ipsa
+        dolores voluptas fugiat assumenda. Molestiae, provident. Consequatur
+        magnam excepturi itaque perspiciatis laborum quo, quibusdam iure est,
+        sequi nam blanditiis molestiae!
+      </p>
+      <div> This is my simple intrest campaound </div>
     </div>
   );
 };
